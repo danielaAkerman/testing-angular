d36804e22038cf2a68eeb2576082fcb788d5b389
M. Daniela Robledo Akerman <danielaakerman14@gmail.com>
2022-10-15 23:39:43 -0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { ButtonsComponent } from './layout/buttons/buttons.component';
+import { LisaComponent } from './layout/lisa/lisa.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ButtonsComponent,
+    LisaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
